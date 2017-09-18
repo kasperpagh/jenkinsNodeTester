e7ab802dec179b34b73f55462422ac27d463af06
Pagh <kasperpagh@gmail.com>
2017-09-18 15:06:33 +0200

    change

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ var server = supertest.agent("http://localhost:4000");
 
 // UNIT test begin
 
-describe("JIMMY unit test",function(){
+describe("JIMMYJAMBA unit test",function(){
 
   // #1 should return home page
   it("should return home page",function(done){
